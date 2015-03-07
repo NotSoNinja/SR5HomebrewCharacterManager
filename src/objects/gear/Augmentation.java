904032d71b30a6232e606b3f2156e859e98dc01a
Johnathan <jwaohnathan@live.com>
2015-03-07 13:23:23 -0500

    I derped fixing the derpage

--- src/objects/gear/Augmentation.java
+++ src/objects/gear/Augmentation.java
@@ -1,12 +1,12 @@
 package objects.gear;
 
 public class Augmentation extends Gear {
-	int rate, cap;
+	int cap;
 	float essenc;
 	//TODO Something to hold mods
 
-	public Augmentation(String namestr, float essence, int capacity, int availability, boolean r, boolean f, int price, String description) {
-		super(namestr, 0, availability, r, f, price, description);
+	public Augmentation(String namestr, int rating, float essence, int capacity, int availability, boolean r, boolean f, int price, String description) {
+		super(namestr, rating, availability, r, f, price, description);
 		cap = capacity;
 		essenc = essence;
 		//TODO Something to hold mods
