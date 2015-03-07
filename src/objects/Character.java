25f7c5f23c5f100c729724046bb7ed1d87950bfd
Johnathan <jwaohnathan@live.com>
2015-03-07 12:07:56 -0500

    removed my homework... woops

--- src/objects/Character.java
+++ src/objects/Character.java
@@ -73,11 +73,612 @@ public class Character {
 	//TODO ArrayList of Ability objects
 	//TODO find someone who wants to implement this
 	
-	public Character(){
+	public Character(String namestr, int startkarma){
 		//TODO An actual constructor
-		
+		player = namestr;
+		karma = totalKarma = startkarma;
 	}
-	
-	//TODO Getters and setters OUT THE WHAZOO
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the ethnicity
+	 */
+	public String getEthnicity() {
+		return ethnicity;
+	}
+
+	/**
+	 * @param ethnicity the ethnicity to set
+	 */
+	public void setEthnicity(String ethnicity) {
+		this.ethnicity = ethnicity;
+	}
+
+	/**
+	 * @return the sex
+	 */
+	public String getSex() {
+		return sex;
+	}
+
+	/**
+	 * @param sex the sex to set
+	 */
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	/**
+	 * @return the bio
+	 */
+	public String getBio() {
+		return bio;
+	}
+
+	/**
+	 * @param bio the bio to set
+	 */
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	/**
+	 * @return the player
+	 */
+	public String getPlayer() {
+		return player;
+	}
+
+	/**
+	 * @param player the player to set
+	 */
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public Metatype getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Metatype type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the stdOfLiving
+	 */
+	public Lifestyle getStdOfLiving() {
+		return StdOfLiving;
+	}
+
+	/**
+	 * @param stdOfLiving the stdOfLiving to set
+	 */
+	public void setStdOfLiving(Lifestyle stdOfLiving) {
+		StdOfLiving = stdOfLiving;
+	}
+
+	/**
+	 * @return the nuyen
+	 */
+	public int getNuyen() {
+		return nuyen;
+	}
+
+	/**
+	 * @param nuyen the nuyen to set
+	 */
+	public void setNuyen(int nuyen) {
+		this.nuyen = nuyen;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the streetCred
+	 */
+	public int getStreetCred() {
+		return streetCred;
+	}
+
+	/**
+	 * @param streetCred the streetCred to set
+	 */
+	public void setStreetCred(int streetCred) {
+		this.streetCred = streetCred;
+	}
+
+	/**
+	 * @return the notariety
+	 */
+	public int getNotariety() {
+		return notariety;
+	}
+
+	/**
+	 * @param notariety the notariety to set
+	 */
+	public void setNotariety(int notariety) {
+		this.notariety = notariety;
+	}
+
+	/**
+	 * @return the awareness
+	 */
+	public int getAwareness() {
+		return awareness;
+	}
+
+	/**
+	 * @param awareness the awareness to set
+	 */
+	public void setAwareness(int awareness) {
+		this.awareness = awareness;
+	}
+
+	/**
+	 * @return the karma
+	 */
+	public int getKarma() {
+		return karma;
+	}
+
+	/**
+	 * @param karma the karma to set
+	 */
+	public void setKarma(int karma) {
+		this.karma = karma;
+	}
+
+	/**
+	 * @return the totalKarma
+	 */
+	public int getTotalKarma() {
+		return totalKarma;
+	}
+
+	/**
+	 * @param totalKarma the totalKarma to set
+	 */
+	public void setTotalKarma(int totalKarma) {
+		this.totalKarma = totalKarma;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public float getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public float getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * @return the bdy
+	 */
+	public int getBdy() {
+		return bdy;
+	}
+
+	/**
+	 * @param bdy the bdy to set
+	 */
+	public void setBdy(int bdy) {
+		this.bdy = bdy;
+	}
+
+	/**
+	 * @return the agi
+	 */
+	public int getAgi() {
+		return agi;
+	}
+
+	/**
+	 * @param agi the agi to set
+	 */
+	public void setAgi(int agi) {
+		this.agi = agi;
+	}
+
+	/**
+	 * @return the rea
+	 */
+	public int getRea() {
+		return rea;
+	}
+
+	/**
+	 * @param rea the rea to set
+	 */
+	public void setRea(int rea) {
+		this.rea = rea;
+	}
+
+	/**
+	 * @return the str
+	 */
+	public int getStr() {
+		return str;
+	}
+
+	/**
+	 * @param str the str to set
+	 */
+	public void setStr(int str) {
+		this.str = str;
+	}
+
+	/**
+	 * @return the wil
+	 */
+	public int getWil() {
+		return wil;
+	}
+
+	/**
+	 * @param wil the wil to set
+	 */
+	public void setWil(int wil) {
+		this.wil = wil;
+	}
+
+	/**
+	 * @return the log
+	 */
+	public int getLog() {
+		return log;
+	}
+
+	/**
+	 * @param log the log to set
+	 */
+	public void setLog(int log) {
+		this.log = log;
+	}
+
+	/**
+	 * @return the intu
+	 */
+	public int getIntu() {
+		return intu;
+	}
+
+	/**
+	 * @param intu the intu to set
+	 */
+	public void setIntu(int intu) {
+		this.intu = intu;
+	}
+
+	/**
+	 * @return the cha
+	 */
+	public int getCha() {
+		return cha;
+	}
+
+	/**
+	 * @param cha the cha to set
+	 */
+	public void setCha(int cha) {
+		this.cha = cha;
+	}
+
+	/**
+	 * @return the edge
+	 */
+	public int getEdge() {
+		return edge;
+	}
+
+	/**
+	 * @param edge the edge to set
+	 */
+	public void setEdge(int edge) {
+		this.edge = edge;
+	}
+
+	/**
+	 * @return the edgePoints
+	 */
+	public int getEdgePoints() {
+		return edgePoints;
+	}
+
+	/**
+	 * @param edgePoints the edgePoints to set
+	 */
+	public void setEdgePoints(int edgePoints) {
+		this.edgePoints = edgePoints;
+	}
+
+	/**
+	 * @return the essence
+	 */
+	public int getEssence() {
+		return essence;
+	}
+
+	/**
+	 * @param essence the essence to set
+	 */
+	public void setEssence(int essence) {
+		this.essence = essence;
+	}
+
+	/**
+	 * @return the magres
+	 */
+	public int getMagres() {
+		return magres;
+	}
+
+	/**
+	 * @param magres the magres to set
+	 */
+	public void setMagres(int magres) {
+		this.magres = magres;
+	}
+
+	/**
+	 * @return the initStd
+	 */
+	public int getInitStd() {
+		return initStd;
+	}
+
+	/**
+	 * @param initStd the initStd to set
+	 */
+	public void setInitStd(int initStd) {
+		this.initStd = initStd;
+	}
+
+	/**
+	 * @return the initMat
+	 */
+	public int getInitMat() {
+		return initMat;
+	}
+
+	/**
+	 * @param initMat the initMat to set
+	 */
+	public void setInitMat(int initMat) {
+		this.initMat = initMat;
+	}
+
+	/**
+	 * @return the initAst
+	 */
+	public int getInitAst() {
+		return initAst;
+	}
+
+	/**
+	 * @param initAst the initAst to set
+	 */
+	public void setInitAst(int initAst) {
+		this.initAst = initAst;
+	}
+
+	/**
+	 * @return the composure
+	 */
+	public int getComposure() {
+		return composure;
+	}
+
+	/**
+	 * @param composure the composure to set
+	 */
+	public void setComposure(int composure) {
+		this.composure = composure;
+	}
+
+	/**
+	 * @return the judgeIntentions
+	 */
+	public int getJudgeIntentions() {
+		return judgeIntentions;
+	}
+
+	/**
+	 * @param judgeIntentions the judgeIntentions to set
+	 */
+	public void setJudgeIntentions(int judgeIntentions) {
+		this.judgeIntentions = judgeIntentions;
+	}
+
+	/**
+	 * @return the memory
+	 */
+	public int getMemory() {
+		return memory;
+	}
+
+	/**
+	 * @param memory the memory to set
+	 */
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+
+	/**
+	 * @return the lift
+	 */
+	public int getLift() {
+		return lift;
+	}
+
+	/**
+	 * @param lift the lift to set
+	 */
+	public void setLift(int lift) {
+		this.lift = lift;
+	}
+
+	/**
+	 * @return the movement
+	 */
+	public int getMovement() {
+		return movement;
+	}
+
+	/**
+	 * @param movement the movement to set
+	 */
+	public void setMovement(int movement) {
+		this.movement = movement;
+	}
+
+	/**
+	 * @return the physLimit
+	 */
+	public int getPhysLimit() {
+		return physLimit;
+	}
+
+	/**
+	 * @param physLimit the physLimit to set
+	 */
+	public void setPhysLimit(int physLimit) {
+		this.physLimit = physLimit;
+	}
+
+	/**
+	 * @return the mentLimit
+	 */
+	public int getMentLimit() {
+		return mentLimit;
+	}
+
+	/**
+	 * @param mentLimit the mentLimit to set
+	 */
+	public void setMentLimit(int mentLimit) {
+		this.mentLimit = mentLimit;
+	}
+
+	/**
+	 * @return the socLimit
+	 */
+	public int getSocLimit() {
+		return socLimit;
+	}
+
+	/**
+	 * @param socLimit the socLimit to set
+	 */
+	public void setSocLimit(int socLimit) {
+		this.socLimit = socLimit;
+	}
+
+	/**
+	 * @return the skills
+	 */
+	public Skill[] getSkills() {
+		return skills;
+	}
+
+	/**
+	 * @param skills the skills to set
+	 */
+	public void setSkills(Skill[] skills) {
+		this.skills = skills;
+	}
+
+	/**
+	 * @return the physDamage
+	 */
+	public int getPhysDamage() {
+		return physDamage;
+	}
+
+	/**
+	 * @param physDamage the physDamage to set
+	 */
+	public void setPhysDamage(int physDamage) {
+		this.physDamage = physDamage;
+	}
+
+	/**
+	 * @return the stunDamage
+	 */
+	public int getStunDamage() {
+		return stunDamage;
+	}
+
+	/**
+	 * @param stunDamage the stunDamage to set
+	 */
+	public void setStunDamage(int stunDamage) {
+		this.stunDamage = stunDamage;
+	}
+
+	/**
+	 * @return the overflow
+	 */
+	public int getOverflow() {
+		return overflow;
+	}
+
+	/**
+	 * @param overflow the overflow to set
+	 */
+	public void setOverflow(int overflow) {
+		this.overflow = overflow;
+	}
 	
 }
--- src/objects/Contact.java
+++ src/objects/Contact.java
@@ -2,10 +2,72 @@ package objects;
 
 public class Contact extends Character {
 	int connection, loyalty, favor;
+	String desc;
+	
+	/**
+	 * @return the connection
+	 */
+	public int getConnection() {
+		return connection;
+	}
 
-	public Contact() {
+	/**
+	 * @param connection the connection to set
+	 */
+	public void setConnection(int connection) {
+		this.connection = connection;
+	}
+
+	/**
+	 * @return the loyalty
+	 */
+	public int getLoyalty() {
+		return loyalty;
+	}
+
+	/**
+	 * @param loyalty the loyalty to set
+	 */
+	public void setLoyalty(int loyalty) {
+		this.loyalty = loyalty;
+	}
+
+	/**
+	 * @return the favor
+	 */
+	public int getFavor() {
+		return favor;
+	}
+
+	/**
+	 * @param favor the favor to set
+	 */
+	public void setFavor(int favor) {
+		this.favor = favor;
+	}
+
+	/**
+	 * @return the desc
+	 */
+	public String getDesc() {
+		return desc;
+	}
+
+	/**
+	 * @param desc the desc to set
+	 */
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Contact(String namestr, int conn, int loylty, String description) {
 		// TODO Auto-generated constructor stub
-		super();
+		super("System", 0);
+		name = namestr;
+		connection = conn;
+		loyalty =loylty;
+		favor = 0;
+		desc = description;
 	}
 
 	//TODO getters and setters
