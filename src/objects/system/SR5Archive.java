0c7b80d12693b5ac27d8ee4ef176906e7e7f04a4
Johnathan <jwaohnathan@live.com>
2015-03-08 16:44:17 -0400

    added some stuff to the archive class to make it useful

--- src/objects/system/SR5Archive.java
+++ src/objects/system/SR5Archive.java
@@ -8,6 +8,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import objects.Quality;
+import objects.Skill;
+import objects.Vehicle;
+import objects.gear.Armor;
+import objects.gear.Augmentation;
+import objects.gear.Cyberdeck;
+import objects.gear.Gear;
+import objects.gear.MeleeWeapon;
+import objects.gear.RangedWeapon;
+
 import com.google.gson.Gson;
 
 /**
@@ -18,14 +28,41 @@ import com.google.gson.Gson;
 public class SR5Archive {
 	File saveLoc;
 	String name;
-	//TODO an array of everything that one could want to save
-	public SR5Archive(String name){
+	int size;
+
+	Skill skills[];
+	protected Quality qualities[];
+	protected Character characters[];
+	protected RangedWeapon rangedWeapons[];
+	protected MeleeWeapon meleeWeapons[];
+	protected Armor armorList[];
+	protected Augmentation augs[];
+	protected Gear gearList[];
+	protected Cyberdeck commlinks[];
+	protected Vehicle vehicles[];
+
+	//Spells, etc.
+	//TODO Array of MagicObject
+	//TODO find someone who wants to implement this
+
+	//Adept Powers
+	//TODO Array of Ability objects
+	//TODO find someone who wants to implement this
+
+	public SR5Archive(String name, int size){
 		saveLoc = new File(name + ".sra");
 		this.name = name;
-		//TODO initialize arrays
+		this.size = size;
+		qualities = new Quality[size];
+		characters = new Character[size];
+		rangedWeapons = new RangedWeapon[size];
+		meleeWeapons = new MeleeWeapon[size];
+		armorList = new Armor[size];
+		augs = new Augmentation[size];
+		gearList = new Gear[size];
+		commlinks = new Cyberdeck[size];
+		vehicles = new Vehicle[size];
 	}
-	
-	//TODO getters/setters?  Maybe protected
 	
 	public boolean writeArchiveToFile(File fi) {
 		if(fi == null){
